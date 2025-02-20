2eb1cce76b0081682bd4ca068de5d92e2b8dbcce
Mike Stefan Wazowsky <fane@kiki.com>
2025-02-19 22:14:19 -0600

    added code file + adj readme.md file to shorter lines

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # num_dig_up2
-given positive integer n, count num times digit k=1, 2, ..., 9  [not 0] appears in all integers btwn 1 and n [inclusive]
+given a positive integer n, count the number of times a digit k = 1, 2, ..., 9
+[not 0] appears in all integers btwn 1 and n [ends 1 and n both included]
 
-for example the count for n=28, k=3 should be 3, because 3 appears once in each of the intermediaries [from 1 to 28]:
-3, 13, 23
-
+for example the count for n=28, k=3 should be 3, because 3 appears once in each
+of the intermediaries [from 1 to 28]: 3, 13, 23
---
+++ num_digs.xgit.py
@@ -0,0 +1,75 @@
+'''
+[+] Count the number of times the digit k appears in all non-negative integers less than or equal to a given integer n.
+
+ex: n=12, k=2 --> 2 [ 2 si 12 ]
+
+unde e:
+https://www.codingame.com/training/easy/number-of-digits
+
+'''
+
+
+def count_slow_but_obvious(n=124, k=1):
+    ans = 0
+    d = '%d' % k  # as digit
+    for m in range(1, n+1):
+        mstr = '%d' % m
+        ans += mstr.count(d)
+    return ans
+
+def show_slow(n=7593, k=3):
+    print('>>> %d in 0 --> %d:  %d times' % (k, n, count_slow_but_obvious(n, k)))
+
+# if measured/tried --> too slow
+
+#-| In [56]: foo(7593824, 3)   --> good test case
+#-| 5597988
+#-| ... foo() ran for: 2.9460 seconds --> do NOT go over millions with this...
+#-| [ on aaea ]
+
+# ============================================================================ +
+
+plan = '''
+will represent numbers as seq/list of digits as written and use darn recursion
+to reduce nnss, ex:
+
+    nl = [5, 0, 2, 7] for 5027  [number-list is the name-hint, if interested!]
+'''
+
+def num(nl): # back from nl -> int format for the number
+    return eval(''.join([str(d) for d in nl]))
+
+def red(nl): # cut-out leading 0's, until at most 1 stays if in digits' place
+    while len(nl) > 1 and nl[0] == 0:
+        nl = nl[1:]
+    return nl
+
+
+def foo4(n=124, k=1):
+    acc = dict() # already-computed-counts (all for the same k, only n is different)
+    def cnt(nl, k): # the tool doing the counting on nl-format input
+        if len(nl) == 1:
+            return 0 if nl[0] < k else 1
+        else:
+            if nl in acc.keys():
+                return acc[nl]
+            td = nl[0]  # top-digit, td != 0
+            n10 = eval('1' + '0'*(len(nl)-1)) # [5, 0, 2, 7] --> p10 = [1, 0, 0, 0]
+            p99 = tuple([9]*(len(nl)-1))       # ...          --> p99 =    [9, 9, 9]
+            rnl = red(nl[1:])           # what's left after split: 5000 + 27
+            acc[p99] = cnt(p99, k) # never compute
+            acc[rnl] = cnt(rnl, k) # these again!!!
+            return \
+                td * acc[p99]  + \
+                ( n10 if k < td else (num(rnl) + 1) if k == td else 0 ) + \
+                acc[rnl]
+
+    znl = tuple([int(d) for d in '%d' % n]) # n --> nl representation
+    return cnt(znl, k)
+
+def show4(n=7593824, k=3):
+    print('<v4>: %d in 0 --> %d:  %d times' % (k, n, foo4(n, k)))
+
+
+# --> run a crazy-large number:
+show4(1234567891011121314151617181920, k=3)   # --> 0.0009s ... awesome!
